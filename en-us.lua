59722d52e01da137053798cdf439f49e76f47dca
Yumouk <Yumou114514@163.com>
2026-06-10 14:39:19 +0800

    Better english wording
    Replace the previous one in localization folder

---
+++ en-us.lua
@@ -0,0 +1,110 @@
+return {
+    descriptions = {
+        Joker={
+            j_stsco_convergence={
+                name = 'Convergence',
+                text = {
+                    'Whenever you {C:red}discard{},',
+                    'a random card held in hand',
+                    'becomes a {C:attention}Gold{} or',
+                    '{C:attention}Steel{} card'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_rucishenhao={
+                name = 'Make It So',
+                text = {
+                    'Every {C:attention}3{} scored cards',
+                    'give {C:red}+#1#{} Mult next',
+                    'scoring, and increases',
+                    'this value by {C:red}+1{}'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_destroy_destroy={
+                name = 'Decisions, Decisions',
+                text = {
+                    'When you use a {C:planet}Planet{}',
+                    'or {C:tarot}Tarot{} card,',
+                    'create two copies',
+                    '(once per Blind)'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_crimsonthorne={
+                name = 'Crimson Mantle',
+                text = {
+                    'Earn {C:money}$8{} at',
+                    'end of round',
+                    '{C:red}+25%{} Blind size'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_BlackYongbao={
+                name = 'Dark Embrace',
+                text = {
+                    'Each sold card',
+                    'gives {C:attention}+1{} hand size,',
+                    'resets at end of round',
+                    '{C:inactive}(Currently +#1#){}'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_stabletongguo={
+                name = 'Unmovable',
+                text = {
+                    'The {C:attention}last{} scoring card',
+                    'gives {C:red}Mult{} equal to',
+                    'twice its {C:blue}Chip{} value'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_2000VOLT={
+                name = 'Voltaic',
+                text = {
+                    '{C:red}+#1#{} Mult,',
+                    'doubles when a',
+                    '{C:attention}Boss Blind{} is defeated'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_form_orm_rm_m={
+                name = 'Echo Form',
+                text = {
+                    'The {C:attention}first{} scoring card',
+                    'each Blind gains a permanent',
+                    '{C:attention}retrigger{}',
+                    '({C:red}self-destructs{} after {C:attention}#1#{} uses)'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+            j_stsco_one_punch={
+                name = 'All For One',
+                text = {
+                    'All {C:attention}Enhanced{} cards are',
+                    'shuffled to the bottom',
+                    'of the deck'
+                },
+                unlock = {
+                    'Unlocked by default.',
+                },
+            },
+        },
+    },
+}
